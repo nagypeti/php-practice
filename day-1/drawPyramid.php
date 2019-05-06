4de82f2530c9399dac24865e887fbaba82be6f96
nagypeti <nagypeti222@gmail.com>
2019-05-06 17:41:36 +0200

    Renamed and deleted description from task

--- day-1/drawPyramid.php
+++ day-1/drawPyramid.php
@@ -1,13 +1,5 @@
 <?php
 
-// Write a program that reads a number from the standard input, then draws a
-// pyramid like this:
-//    *
-//   ***
-//  *****
-// *******
-// The pyramid should have as many lines as the number was
-
 function drawPyramid($height)
 {
     $pyramid = [];

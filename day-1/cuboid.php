7f06333e4ffbdeb94642b8c65d729369f0f8b5c4
nagypeti <nagypeti222@gmail.com>
2019-05-06 14:34:32 +0200

    Cuboid task works as expected

---
+++ day-1/cuboid.php
@@ -0,0 +1,44 @@
+<?php
+
+$calcVolume = function($length, $width, $height) {
+    return ($length * $width * $height);
+};
+
+$calcSurface = function($length, $width, $height) {
+    return (2 * $width * $length) + (2 * $length * $height) + (2* $height * $width);
+};
+
+if(isset($_POST['calculate'])) {
+    $volume = $calcVolume($_POST['length'], $_POST['width'], $_POST['height']);
+    $surface = $calcSurface($_POST['length'], $_POST['width'], $_POST['height']);
+}
+    
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Calculate the surface and the volume of a cuboid:</h1>
+    <?php if(!isset($_POST['calculate'])) { ?>
+        <?php echo "<p>Please provide input!</p>"; ?>
+    <?php } ?>
+    <?php if(isset($_POST['calculate'])) { ?>
+        <?php echo "<h1>The volume of your cuboid: $volume</h1>"; ?>
+        <?php echo "<h1>The surface of your cuboid: $surface</h1>"; ?>
+    <?php } ?>
+    <form method="post">
+        <label for="length">Length:</label>
+        <input type="text" name="length" id="length" required>
+        <label for="height">Heigth:</label>
+        <input type="text" name="height" id="height" required>
+        <label for="width">Width:</label>
+        <input type="text" name="width" id="width" required>
+        <button type="submit" name="calculate">Calculate, please!</button>
+    </form>
+</body>
+</html>

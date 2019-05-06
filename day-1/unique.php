bdd789005fafcf361b93dd0f2a6815b457b7c394
nagypeti <nagypeti222@gmail.com>
2019-05-06 21:36:52 +0200

    Unique works as expected

---
+++ day-1/unique.php
@@ -0,0 +1,19 @@
+<?php
+
+function unique($array)
+{
+    $temp_array = array();
+    $i = 0;
+    
+    foreach ($array as $val) {
+        if (!in_array($val, $temp_array)) {
+            $temp_array[] = $val;
+        }
+        $i++;
+    }
+    return $temp_array;
+}
+
+echo '<pre>', var_dump(unique([1, 11, 34, 11, 52, 61, 1, 34])), '</pre';
+
+?>

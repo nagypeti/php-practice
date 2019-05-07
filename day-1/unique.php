c490b355831d848535803bc38cccbdb83a9be0f8
nagypeti <nagypeti222@gmail.com>
2019-05-07 09:14:21 +0200

    Assigned function input to variable

--- day-1/unique.php
+++ day-1/unique.php
@@ -1,5 +1,7 @@
 <?php
 
+$input = [1, 11, 34, 11, 52, 61, 1, 34];
+
 function unique($array)
 {
     $temp_array = array();
@@ -14,6 +16,6 @@ function unique($array)
     return $temp_array;
 }
 
-echo '<pre>', var_dump(unique([1, 11, 34, 11, 52, 61, 1, 34])), '</pre';
+echo '<pre>', var_dump(unique($input)), '</pre';
 
 ?>

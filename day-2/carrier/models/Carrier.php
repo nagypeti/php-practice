97e3b07e344ead55e3dff4226606937571e92e7c
nagypeti <nagypeti222@gmail.com>
2019-05-07 15:16:19 +0200

    Structure of carrier created

--- day-2/carrier/models/Carrier.php
+++ day-2/carrier/models/Carrier.php
@@ -1,11 +1,37 @@
 <?php
 
+require_once '../classes.php';
 class Carrier
 {
 
-    public function __construct()
+    private $aircrafs;
+    private $ammo;
+    private $health;
+
+    public function __construct($ammo = 100, $health = 1000)
+    {
+        $this->ammo = $ammo;
+        $this->health = $health;
+    }
+
+    public function add()
     {
 
+    }
+
+    public function fill()
+    {
+        
+    }
+
+    public function fight()
+    {
+        
+    }
+
+    public function getStatus()
+    {
+        
     }
 
 }

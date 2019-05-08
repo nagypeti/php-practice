60444b24ebd4b95c59ba7dccec5d78c8b7b88f83
nagypeti <nagypeti222@gmail.com>
2019-05-08 22:39:20 +0200

    checkstyle, removed empty line

--- day-1/bmi.php
+++ day-1/bmi.php
@@ -3,10 +3,8 @@
 $massInKg = 81.2;
 $heightInM = 1.78;
 
-
 $bmi = function($massInKg, $heightInMeter) {
     return number_format($massInKg / ($heightInMeter ** 2), 2);
 };
 
 echo "Your bmi index is: {$bmi($massInKg, $heightInM)}";
-?>

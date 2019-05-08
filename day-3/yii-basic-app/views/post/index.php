3ccb9cec5491b2cdc360b982b23bf77bbbc28fb5
nagypeti <nagypeti222@gmail.com>
2019-05-08 22:14:07 +0200

    Added more bootstrap styling and Submit new post button, add post now rendered on separete view

--- day-3/yii-basic-app/views/post/index.php
+++ day-3/yii-basic-app/views/post/index.php
@@ -6,32 +6,37 @@ use yii\helpers\Html;
 ?>
 
 <h1>Trending posts</h1>
+
+<a href="/post/add" class="btn btn-primary">Submit new post</a>
+
 <table class="table table-hover">
+    <tr>
+        <td>Likes</td>
+        <td>Up</td>
+        <td>Down</td>
+        <td>Title</td>
+        <td>Edit</td>
+        <td>Delete</td>
+    </tr>
     <?php foreach ($posts as $post): ?>
         <tr>
-            <td><?= Html::encode($post->likes) ?></td>
-            <td>
-                <a href="/post/upvote?id=<?= $post->id ?>"><i class="material-icons">arrow_drop_up</i></a>
+            <td style="width: 5%"><?= Html::encode($post->likes) ?></td>
+            <td style="width: 5%">
+                <a href="/post/upvote?id=<?= $post->id ?>"><i class="large material-icons" role="button">arrow_drop_up</i></a>
             </td>
-            <td>
-                <a href="/post/downvote?id=<?= $post->id ?>"><i class="material-icons">arrow_drop_down</i></a>
+            <td style="width: 5%">
+                <a href="/post/downvote?id=<?= $post->id ?>"><i class="large material-icons" role="button">arrow_drop_down</i></a>
             </td>
-            <td>
+            <td style="width: 75%">
                 <a href="<?= $post->content ?>"><?= Html::encode($post->title) ?></a>
             </td>
-            <td>
-                <a href="/post/edit?id=<?= $post->id ?>"><i class="material-icons">edit</i></a>
+            <td style="width: 5%">
+                <a href="/post/edit?id=<?= $post->id ?>"><i class="large material-icons" role="button">edit</i></a>
             </td>
-            <td>
-                <a href="/post/delete?id=<?= $post->id ?>"><i class="material-icons">delete</i></a>
+            <td style="width: 5%">
+                <a href="/post/delete?id=<?= $post->id ?>"><i class="large material-icons" role="button">delete</i></a>
             </td>
         </tr>
     <?php endforeach; ?>
 </table>
-
-<?= $this->render('_postForm', [
-    'postForm' => $postForm,
-    'buttonText' => 'Add',
-    'action' => ['post/add']
-]) ?>
 

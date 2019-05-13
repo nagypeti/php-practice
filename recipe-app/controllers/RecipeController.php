fa3160c3f574ae0d5afc511c7d268732339eeba8
nagypeti <nagypeti222@gmail.com>
2019-05-13 21:50:38 +0200

    Feat: add, save, index and showrecipebyid refactor and works as expected

--- recipe-app/controllers/RecipeController.php
+++ recipe-app/controllers/RecipeController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
+use app\models\Ingredient;
+use app\models\IngredientForm;
 use app\models\Recipe;
 use app\models\RecipeForm;
 
@@ -12,14 +14,11 @@ class RecipeController extends Controller
 
     public function actionIndex()
     {
-        $recipes = new Recipe([
-            'name' => 'asd',
-            'description' => 'some',
-            'ingredients' => 'onion'
-        ]);
+        $recipes = Recipe::find()
+            ->all();
 
         return $this->render('index', [
-            'recipe' => $recipes,
+            'recipes' => $recipes,
         ]);
     }
 
@@ -29,21 +28,42 @@ class RecipeController extends Controller
             return $this->redirect(Yii::$app->urlManager->createUrl("site/login"));
         }else{
             return $this->render('add', [
-                'recipeForm' => new RecipeForm()
+                'recipeForm' => new RecipeForm(),
+                'ingredientForm' => new IngredientForm()
             ]);
         }
     }
 
     public function actionSave()
     {
-        $data = \Yii::$app->request->post();
+        $data = Yii::$app->request->post();
+
         $recipe = new Recipe();
+        $ingredients = [];
+
         $recipe->attributes = $data['RecipeForm'];
-        if ($recipe->save()) {
-            return $this->redirect('index');
-        } else {
-            var_dump($recipe->errors);
+        $recipe->save();
+        $ingredientsArray = explode('\r\n',$data['IngredientForm']['name']);
+        $length = count($ingredientsArray);
+
+        for ($i=0; $i < $length; $i++) { 
+            $ingredients[$i] = new Ingredient();
+            $ingredients[$i]->name = $ingredientsArray[$i];
+            $ingredients[$i]->save();
+            $ingredients[$i]->link('recipes', $recipe);
         }
+        
+        return $this->redirect('/recipes');
+    }
+
+    public function actionShowrecipebyid($id)
+    {
+        $recipe = Recipe::find()
+            .where(['id' => $id])
+            .one();
+
+        $ingredients = $recipe->ingredients;
+
     }
 
 
